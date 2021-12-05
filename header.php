4a41d226241f9eddddc63f2a72c950e0cecf7212
Charles Harvey <harvey.charles@gmail.com>
2021-12-05 17:36:27 +0000

    add google analytics

--- header.php
+++ header.php
@@ -1,55 +1,67 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php
-    /*
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php
+          /*
      * Print the <title> tag based on what is being viewed.
      * We filter the output of wp_title() a bit -- see
      * twentyten_filter_wp_title() in functions.php.
      */
-    wp_title('|', true, 'right');
+          wp_title('|', true, 'right');
 
-    ?></title>
-    <link rel="profile" href="http://gmpg.org/xfn/11"/>
-    <link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.png" type="image/x-icon">
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
-    <?php wp_head(); ?>
-  </head>
+          ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.png" type="image/x-icon">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <?php wp_head(); ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-7JF1WCZMDL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-  <body <?php body_class(); ?>>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-    <header class="header" id="header">
-      <nav>
+    gtag('config', 'G-7JF1WCZMDL');
+  </script>
+</head>
 
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-3 ">
+<body <?php body_class(); ?>>
+
+  <header class="header" id="header">
+    <nav>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3 ">
+            <ul>
+              <li>
+                <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-sm-9">
+            <div id="navigation_menu">
               <ul>
-                <li>
-                  <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
-                </li>
+                <?php chilly_nav('primary'); ?>
               </ul>
             </div>
-            <div class="col-sm-9">
-              <div id="navigation_menu">
-                <ul>
-                  <?php chilly_nav('primary'); ?>
-                </ul>
-              </div>
-            </div>
-
           </div>
-          <a href="#" id="menu_button">Menu</a>
+
         </div>
+        <a href="#" id="menu_button">Menu</a>
+      </div>
 
-      </nav>
-    </header>
+    </nav>
+  </header>
 
 
 
-    <div class="container">
+  <div class="container">
 
-      <div id="main">
+    <div id="main">
